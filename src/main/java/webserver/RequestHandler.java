64d3e396b29a92ca55fa16275e7283e9c991cb8c
Go-Jaecheol <gojaech@naver.com>
2022-02-03 14:15:24 +0900

    Clear Requirement 1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +22,22 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            log.debug("line: {}", line);
+            if (line == null) {
+                return;
+            }
+            String[] tokens = line.split(" ");
+            String url = tokens[1];
+            while (!"".equals(line)) {
+                line = br.readLine();
+                log.debug("line: {}", line);
+            }
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "New message!!!!!".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
